a867ba6fe0b01a47d72b269d6a73ad7b3b68fe54
CosmoMougins <vous2@hotmail.fr>
2025-03-31 23:24:32 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,12 @@
+// Remplacez les valeurs ci-dessous par celles de votre projet Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBzKYZFVo4935ffEwvD8G2gYCb3i8RMkXU",
+  authDomain: "livraison-cosmosushimougins.firebaseapp.com",
+  projectId: "livraison-cosmosushimougins",
+  storageBucket: "livraison-cosmosushimougins.firebasestorage.app",
+  messagingSenderId: "977101512051",
+  appId: "1:977101512051:web:04aeda0d6956ac1337b906"
+};
+
+firebase.initializeApp(firebaseConfig);
 
